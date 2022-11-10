a57647ac9f1aa4e84e716a85f732e295d636e4f0
igdanit <vov4ikmolo4ik@gmail.com>
2022-11-10 17:01:09 +0600

    first commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const password = ref('');
+    const email = ref('');
+
+    function onSubmit(event) {
+        console.dir(event.target)
+        console.log(password.value)
+        console.log(email.value)
+    }
+</script>
+
+<template>
+    <div class=sign-up>
+        <div><p>Создать учëтную запись</p></div>
+    </div>
+    <form @submit.prevent="onSubmit">
+        <input v-model="email" type="email" placeholder="Введите email">
+        <input v-model="password" type="password" placeholder="Введите пароль">
+        <button type="submit">Зарегестрироваться</button>
+    </form>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/signIn.vue
@@ -0,0 +1,21 @@
+<script>
+   import { ref } from 'vue'
+
+    const password = ref('');
+    const email = ref('');
+</script>
+
+<template>
+    <div class=sign-in>
+        <div><p>Войти</p></div>
+    </div>
+    <form @submit.prevent="onSubmit">
+        <input v-model="email" type="email" placeholder="Введите email">
+        <input v-model="password" type="password" placeholder="Введите пароль">
+        <button type="submit">Войти</button>
+    </form>
+</template>
+
+<style>
+
+</style>
