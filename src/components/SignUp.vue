b99d8806caad99e3c947d07555477d3cd8aca6de
igdanit <vov4ikmolo4ik@gmail.com>
2022-11-10 22:03:05 +0600

    signUp component refactored

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,24 +2,30 @@
     import { ref } from 'vue'
 
     const password = ref('');
+    const repeatedPassword = ref('');
     const email = ref('');
+    const username = ref('');
 
+    // data correctness check
     function onSubmit(event) {
-        console.dir(event.target)
-        console.log(password.value)
-        console.log(email.value)
+        if (password !== repeatedPassword) {
+            event.preventDefault()
+            alert('Not implemented')
+        }
     }
+
 </script>
 
 <template>
     <div class=sign-up>
-        <div><p>Создать учëтную запись</p></div>
+        <form @submit="onSubmit">
+            <input v-model="username" name="username" type="text" placeholder="Введите имя пользователя">
+            <input v-model="email" name="email" type="email" placeholder="Введите email">
+            <input v-model="password" name="password" type="password" placeholder="Введите пароль">
+            <input v-model="repeatedPassword" name="repeated-password" type="password" placeholder="Повторно введите пароль ">
+            <button type="submit">Зарегестрироваться</button>
+        </form>
     </div>
-    <form @submit.prevent="onSubmit">
-        <input v-model="email" type="email" placeholder="Введите email">
-        <input v-model="password" type="password" placeholder="Введите пароль">
-        <button type="submit">Зарегестрироваться</button>
-    </form>
 </template>
 
 <style>
