43c435bee4384ee4dde60f0ada418b38908260d5
igdanit <vov4ikmolo4ik@gmail.com>
2022-11-17 20:56:38 +0600

    Added UI compinents

---
+++ src/components/UI/FieldInput.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+defineProps(['modelValue'])
+defineEmits(['update:modelValue'])
+
+</script>
+
+<template>
+    <input class="input" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+</template>
+
+<style>
+    .input {
+        margin: 10px;
+        box-sizing: border-box;
+        width: 95%;
+    }
+</style>
